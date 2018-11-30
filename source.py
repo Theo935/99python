5e86e4eac6c9748761e0f43a97d35a68c967dab6
changxu3577 <44255145+changxu3577@users.noreply.github.com>
2018-11-30 16:57:53 +0800

    Create source.py

---
+++ source.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.5
+i=0
+while i <= 9:
+    j = 1
+    while j <= i:
+        print(str(i) + "*" + str(j) +  "=" +  str(i*j),end=" ")
+        j+=1
+    print()
+    i+=1
+print()
